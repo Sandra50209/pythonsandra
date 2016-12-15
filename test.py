47903b441b433d379cfe18358ee77f44c18ed6e3
Sandra Thomaschewski <thomaschewskisandra@gmail.comgit config --global user.name Sandra>
2016-12-15 17:06:43 +0100

    game

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-secret = "10"
-raw_input ("guess")
+secret = 10
+guess = int(raw_input("secret: "))
+wrong = "Nope! Again please!"
+right =  "Wow!!! Great!"
 
-if
+if guess == secret:
+    print right
+else:
+    print wrong
