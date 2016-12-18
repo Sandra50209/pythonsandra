3a7f938632a8d2d2cc05aa1361bf744ab583ad0d
Sandra Thomaschewski <thomaschewskisandra@gmail.comgit config --global user.name Sandra>
2016-12-18 20:02:20 +0100

    FizzbuzzNEU

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,5 +1,10 @@
 guess = int(raw_input("Select a number between 1 and 100:"))
 
+if guess > 100:
+    print "error"
+if guess < 1:
+    print "error"
+    #wenn ich eine Zahl ausserhalb von 1-100 schreib, bringt er mir einfach die ganze Zahlenreihe, wie kann ich das eingrenzen?
 for i in range (1,100):
     if i % 3 != 0 and i % 5 !=0:
         print i
@@ -11,3 +16,4 @@ for i in range (1,100):
         print "fizzbuzz"
     if i == guess:
         break
+
